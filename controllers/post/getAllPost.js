af315a451fcc22d9f7d857cd599831892c5365d0
KE-NETIZEN-OOPS <abedmach13@gmail.com>
2024-12-24 10:01:33 +0300

    Added get all posts and get post by ID endpoints

--- controllers/post/getAllPost.js
+++ controllers/post/getAllPost.js
@@ -1,1 +1,21 @@
 // Get all posts
+const Post = require('../models/postModel');
+const handleErrors = require('../utils/handleErrors');
+
+/**
+ * Controller to retrieve all posts.
+ */
+const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find(); // Fetch all posts
+    if (!posts.length) {
+      return res.status(404).json({ success: false, message: 'No posts found' });
+    }
+
+    res.status(200).json({ success: true, data: posts });
+  } catch (error) {
+    handleErrors(res, error, 'Failed to retrieve posts');
+  }
+};
+
+module.exports = getAllPosts;
--- controllers/post/getPostById.js
+++ controllers/post/getPostById.js
@@ -1,1 +1,23 @@
 // Get a post by id
+const Post = require('../models/postModel');
+const handleErrors = require('../utils/handleErrors');
+
+/**
+ * Controller to retrieve a post by its ID.
+ */
+const getPostById = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const post = await Post.findById(postId); // Find post by ID
+
+    if (!post) {
+      return res.status(404).json({ success: false, message: 'Post not found' });
+    }
+
+    res.status(200).json({ success: true, data: post });
+  } catch (error) {
+    handleErrors(res, error, 'Failed to retrieve post');
+  }
+};
+
+module.exports = getPostById;
