56ad72773f00d3eab827b881cf845cc3d5dd4fd4
Abdelaziz <abdeez.aushar@yandex.com>
2024-12-24 16:22:08 +0300

    Fixes
    installed nodemon
    (npm start)

--- routes/job.js
+++ routes/job.js
@@ -3,7 +3,7 @@ const express = require('express');
 const jobRouter = express.Router();
 const verifyToken = require('../middleware/auth/verifyToken')
 
-jobRouter.post('/', verifyToken, require('../controllers/job/createJob'));
+jobRouter.post('/create-job', verifyToken, require('../controllers/job/createJob'));
 // jobRouter.get('/', require('../controllers/job/getAllJob'));
 // jobRouter.get('/:id', require('../controllers/job/getJobById'));
 // jobRouter.put('/:id', require('../controllers/job/updateJob'));
---
+++ utils/job/createJobValidation.js
@@ -0,0 +1,13 @@
+const Joi = require('joi')
+
+function validateCreateJob(data) {
+    const schema = Joi.object({
+        description: Joi.string().required().messages({ 'any.required': 'Description Required' }),
+        minReq: Joi.string().required().messages({ 'any.required': 'Minimum Requirement Required' }),
+        category: Joi.string().required().valid('Technology', 'Finance', 'Education', 'Healthcare').messages({ 'any.required': 'Category Required' }),
+        imageURL: Joi.string().optional(),
+    })
+    return schema.validate(data);
+}
+
+module.exports = validateCreateJob 
