f160212cb2e4472b264cea6fc2ce756708002e35
Omkar Prajapati <omkarprajapati3625@gmail.com>
2024-12-24 21:47:45 +0530

    Get User by Id

--- controllers/user/getUserById.js
+++ controllers/user/getUserById.js
@@ -1,1 +1,20 @@
 // Get a user by Id
+const User = require('../../models/User');
+
+// Get user details by ID
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params
+    
+    const user = await User.findById(id).select('-password'); // Exclude password
+    if (!user) return res.status(404).json({ Status: 0, Message: 'User not found' });
+    
+    return res.status(200).json({ Status: 1,Message:"User Found", Data: user });
+  } catch (error) {
+    return res.status(500).json({ Status: 0, Message: 'Error fetching user', error });
+  }
+};
+
+module.exports = {
+    getUserById
+}
--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,15 @@
 // All the user routes
+const express = require('express');
+const router = express.Router();
+const verifyToken = require('../middleware/auth/verifyToken');
+
+// Import Controller 
+const { getUserById } = require('../controllers/user/getUserById');
+
+// Routes for Users
+router.get('/:id', verifyToken, getUserById); // Get User by Id
+
+
+module.exports = router;
+
+
