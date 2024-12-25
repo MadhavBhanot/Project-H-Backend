e4c3adf2039508b0b80d4dc78355cb2807f1c689
Abdelaziz <abdeez.aushar@yandex.com>
2024-12-25 16:46:42 +0300

    Apply for a job done
    added main middleware to check for id in query params

--- controllers/job/applyForAJob.js
+++ controllers/job/applyForAJob.js
@@ -1,1 +1,16 @@
 // Apply for a job
+const Job = require('../../models/Job')
+const User = require('../../models/User')
+
+async function applyForAJob(req, res) {
+    let job = await Job.findById(req.params.id)
+    if (!job) return res.status(400).json({ message: "Job Not Found" })
+    job.applicants.push(req.user.userId)
+    let user = await User.findById(req.user.userId)
+    user.appliedJobs.push(job._id)
+    await job.save()
+    await user.save()
+    return res.status(200).json({ message: "Application Sent Successfully" })
+}
+
+module.exports = applyForAJob;
--- routes/job.js
+++ routes/job.js
@@ -2,13 +2,14 @@
 const express = require('express');
 const jobRouter = express.Router();
 const verifyToken = require('../middleware/auth/verifyToken')
+const checkObjectID = require('../middleware/main/checkObjectID')
 
 jobRouter.post('/create-job', verifyToken, require('../controllers/job/createJob'));
 jobRouter.get('/', verifyToken, require('../controllers/job/getAllJob'));
-jobRouter.get('/:id', verifyToken, require('../controllers/job/getJobById'));
-jobRouter.delete('/:id', verifyToken, require('../controllers/job/deleteJob'));
-jobRouter.patch('/:id', verifyToken, require('../controllers/job/updateJob'));
-jobRouter.get('/:id/applicants', verifyToken, require('../controllers/job/getApplicants'));
-// jobRouter.post('/:id/apply', require('../controllers/job/applyForAJob'));
+jobRouter.get('/:id', verifyToken, checkObjectID, require('../controllers/job/getJobById'));
+jobRouter.delete('/:id', verifyToken, checkObjectID, require('../controllers/job/deleteJob'));
+jobRouter.patch('/:id', verifyToken, checkObjectID, require('../controllers/job/updateJob'));
+jobRouter.get('/:id/applicants', verifyToken, checkObjectID, require('../controllers/job/getApplicants'));
+jobRouter.post('/:id/apply', verifyToken, checkObjectID, require('../controllers/job/applyForAJob'));
 
 module.exports = jobRouter;
