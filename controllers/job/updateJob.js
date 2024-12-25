4630f1bcebede2ab97f05f1b59e02cd669953978
Abdelaziz <abdeez.aushar@yandex.com>
2024-12-25 11:01:46 +0300

    Update Job Done

--- controllers/job/updateJob.js
+++ controllers/job/updateJob.js
@@ -1,20 +1,17 @@
 // Update a job
 const Job = require('../../models/Job')
-const User = require('../../models/User')
 
 async function updateJob(req, res) {
     const job = await Job.findById(req.params.id)
-    if (!req.user.isVerified) return res.status(400).json({ message: "Not Verified To Delete a Job" })
+    if (!req.user.isVerified) return res.status(400).json({ message: "Not Verified To Update a Job" })
     if (!job) return res.status(400).json({ message: "Job Not Found" })
-    if (job.postedBy.equals(req.user.userId)) await updateJobHelper(req.user.userId, job)
-    return res.status(200).json({ message: "Job Deleted Successfully" })
+    if (job.postedBy.equals(req.user.userId)) await updateJobHelper(job, req.body)
+    return res.status(200).json({ message: "Job Updated Successfully" })
 }
 
-async function updateJobHelper(userId, job) {
-    await Job.deleteOne(job);
-    let user = await User.findById(userId)
-    user.postedJobs.pull(job._id)
-    await user.save();
+async function updateJobHelper(job, data) {
+    await job.updateOne(data)
+    await job.save()
 }
 
 module.exports = updateJob;
