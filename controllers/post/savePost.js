49e029f7120bbf58c0c58f93e28aa8ec5868fb4f
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2024-12-26 17:27:30 +0530

    Save Post Done

---
+++ controllers/post/savePost.js
@@ -0,0 +1,40 @@
+const Post = require('../../models/Post')
+const User = require('../../models/User')
+
+const savePost = async (req, res) => {
+  const id = req.params.id
+  if (!id) {
+    return res.status(400).json({ message: 'Post Id is Required' })
+  }
+  const userId = req.user.userId
+
+  try {
+    const post = await Post.findById(id)
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' })
+    }
+
+    const user = await User.findById(userId)
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    // Check if the post is already saved by the user
+    if (user.savedPosts.includes(id)) {
+      // Unsave the post
+      user.savedPosts.pull(id)
+      await user.save()
+      return res.status(200).json({ message: 'Post unsaved successfully' })
+    } else {
+      // Save the post
+      user.savedPosts.push(id)
+      await user.save()
+    }
+
+    return res.status(200).json({ message: 'Post saved successfully' })
+  } catch (error) {
+    return res.status(500).json({ message: 'Error saving post' })
+  }
+}
+
+module.exports = savePost
--- routes/post.js
+++ routes/post.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const verifyToken = require('../middleware/auth/verifyToken.js')
 
 // Import controllers
-// const createPost = require('../controllers/createPost')
+
 const getAllPosts = require('../controllers/post/getAllPost.js')
 const getPostById = require('../controllers/post/getPostById.js')
 const updatePost = require('../controllers/post/updatePost.js')
@@ -12,7 +12,7 @@ const deletePost = require('../controllers/post/deletePost.js')
 const likePost = require('../controllers/post/likeOrUnlikePost.js')
 const updatePost = require('../controllers/post/updatePost.js')
 const addComment = require('../controllers/post/addComment.js')
-// const savePost = require('../controllers/savePost')
+const savePost = require('../controllers/post/savePost.js')
 
 // Routes for posts
 router.post('/create', verifyToken, createPost) // Create a new post
@@ -21,8 +21,8 @@ router.get('/:id', getPostById) // Get a specific post
 router.patch('/update/:id', verifyToken, updatePost) // Update a post
 router.delete('/delete/:id', verifyToken, deletePost) // Delete a post
 router.post('/like/:id', verifyToken, likePost) // Like or unlike a post
-// router.post('/save/:id', savePost) // Save a post
-router.get('/:id/comments', getAllComment) // Get all comments for a post
-router.post('/:id/comment', addComment) // Add a comment to a post
+router.post('/save/:id', savePost) // Save a post
+router.get('/comments/:id', getAllComment) // Get all comments for a post
+router.post('/comment/:id', verifyToken, addComment) // Add a comment to a post
 
 module.exports = router
