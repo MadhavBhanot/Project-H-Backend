cab2eacdf4c8517c5c8f91bca4d89ec33bda964c
MadhavBhanot <mdhvbhanot@gmail.com>
2025-03-23 19:39:01 +0530

    delete story addon

--- routes/story.js
+++ routes/story.js
@@ -6,6 +6,7 @@ const { verifyClerkToken } = require('../middleware/clerk/verifyToken');
 const createStory = require('../controllers/story/createStory');
 const getStories = require('../controllers/story/getStories');
 const interactWithStory = require('../controllers/story/interactWithStory');
+const deleteStory = require('../controllers/story/deleteStory');
 
 // Protected routes - require authentication
 storyRouter.use(verifyClerkToken);
@@ -14,11 +15,13 @@ storyRouter.use(verifyClerkToken);
 storyRouter.get('/feed', getStories);
 storyRouter.post('/create', createStory);
 storyRouter.post('/:id/interact', interactWithStory);
+storyRouter.delete('/:id', deleteStory);
 
 // Log the available routes on startup
 console.log('📋 Story routes mounted:');
 console.log('- GET /api/stories/feed');
 console.log('- POST /api/stories/create');
 console.log('- POST /api/stories/:id/interact');
+console.log('- DELETE /api/stories/:id');
 
 module.exports = storyRouter; 
